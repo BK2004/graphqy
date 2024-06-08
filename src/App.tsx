dadd2c886d0ae9cd79a963cb06eb66a4cfa19b43
BK2004 <thunder10.bk@gmail.com>
2024-06-08 16:24:18 -0500

    basic code editor finished. switched to textarea to deal with paste issues and entities. implemented line numbering.

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import CodeEditor from './components/CodeEditor';
 function App() {
 return (
 	<div className="App p-4 h-screen overflow-y-auto">
-		<CodeEditor init={"hi"} />
+		<CodeEditor />
 	</div>
 );
 }
--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -1,9 +1,5 @@
 import { useRef } from "react";
 
-interface Props {
-	init: string
-}
-
 const KEYWORDS = [
 	"let",
 	"const",
@@ -21,7 +17,7 @@ const isEntity = (() => {
 })();
 
 const highlightString: (input: string) => string = (input: string) => {
-	return input.replaceAll(/(<div>.*?<\/div>|&[a-zA-Z0-9]+;|".*"|[a-zA-Z][a-zA-Z0-9_]*|[0-9]+)/g, (str) => {
+	return input.replaceAll(/(<div>.*?<\/div>|".*?"|&[a-zA-Z0-9]+;|[a-zA-Z][a-zA-Z0-9_]*|[0-9]+)/g, (str) => {
 		if (/<div>.*?<\/div>/.test(str)) {
 			// Only highlight what is inside of divs
 			return "<div>" + highlightString(str.substring(5, str.length - 6)) + "</div>";
@@ -47,28 +43,33 @@ const highlightString: (input: string) => string = (input: string) => {
 	});
 }
 
-const CodeEditor = ({ init }: Props) => {
+const CodeEditor = () => {
 	const highlightedRef = useRef<HTMLDivElement>(null);
 
 	const onInput = (input: string) => {
 		if (!highlightedRef.current) return;
 
-		const highlightedLines = input.replaceAll(/<div><br><\/div>/g, "<br>").split("<br>").map((str) => {
+		const highlightedLines = input.split("\n").map((str) => {
 			const h = highlightString(str);
 			return h;
 		});
 
-		highlightedRef.current.innerHTML = highlightedLines.join("<br>");
+		highlightedRef.current.innerHTML = highlightedLines.map((v, i) => {
+			return `<span class="relative line-num">${v}</span>`;
+		}).join("<br>");
 	}
 
 	return (<div className="w-full h-fit relative bg-gray-200">
-		<div className="appearance-none inline-block relative w-full h-max focus:border-none focus:outline-none bg-transparent text-transparent caret-black z-10"
-			contentEditable
+		<textarea className="appearance-none px-7 resize-none inline-block relative w-full overflow-y-auto break-words min-h-4 focus:border-none focus:outline-none bg-transparent text-transparent caret-black z-10"
 			onInput={(e) => {
-				onInput(e.currentTarget.innerHTML);
+				e.currentTarget.style.height = "1px";
+				e.currentTarget.style.height = (25 + e.currentTarget.scrollHeight) + "px";
+				onInput(e.currentTarget.value);
 			}}>
-		</div>
-		<div className="code-highlight absolute w-full h-full top-0 left-0 select-none" ref={highlightedRef}></div>
+		</textarea>
+		<pre>
+			<div className="code-highlight absolute w-full h-full top-0 left-0 select-none" ref={highlightedRef}></div>
+		</pre>
 	</div>)
 }
 
